482261b2dacc951567ef1e29602d6fe3606eea02
comcanned <jordanmrodgers1@hotmail.com>
2024-11-13 16:02:49 -0500

    Update main.c

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ int main(int argc, char* argv[]) {
     const char* filename = argv[1];
     int pass1_only = (argc == 3 && strcmp(argv[2], "--pass1only") == 0);
 
+    // Open the input file for reading
     FILE* inputFile = fopen(filename, "r");
     if (!inputFile) {
         printf("Error: Could not open file %s\n", filename);
@@ -20,19 +21,30 @@ int main(int argc, char* argv[]) {
 
     int LOCCTR = 0;
 
-    // Pass 1: Generate Symbol Table
-    pass1_generate_symbol_table(inputFile, &LOCCTR);
+    // Run Pass 1: Generate Symbol Table
+   if (!pass1_generate_symbol_table(inputFile, &LOCCTR)) {
+    printf("Assembly error detected in Pass 1. Object file was not created.\n");
+    fclose(inputFile);
+    return 1;
+}
 
+
+    // If only Pass 1 is requested, print the symbol table and exit
     if (pass1_only) {
-        // Output symbol table if only running Pass 1
         print_symbol_table();
         fclose(inputFile);
         return 0;
     }
 
-    // Pass 2: Generate Object Code
-    pass2_generate_object_code(inputFile, LOCCTR, filename);
+    // Run Pass 2: Generate Object Code
+   if (!pass2_generate_object_code(inputFile, LOCCTR, filename)) {
+    printf("Assembly error detected in Pass 2. Object file was not created.\n");
+    fclose(inputFile);
+    return 1;
+}
 
+
+    // Close the input file after both passes are complete
     fclose(inputFile);
     return 0;
 }
